53b9fb5cffc27382174430fc0bbee0100ce154d4
BlackHack13 <98903391+BlackHack13@users.noreply.github.com>
2023-02-11 01:40:46 +0100

    Update PDF Shuffel.py

--- PDF Shuffel.py
+++ PDF Shuffel.py
@@ -6,28 +6,26 @@ import os
 folder = 'PDF'
 pdf_files = [f for f in os.listdir(folder) if f.endswith('.pdf')]
 
+# Neue PDF-Datei erstellen
+pdf_writer = PyPDF2.PdfWriter()
+
 # Alle PDF-Dateien durchlaufen und mischen
 for pdf_file_name in pdf_files:
     # PDF-Datei öffnen
-    pdf_file = open(os.path.join(folder, pdf_file_name), 'rb')
-    pdf_reader = PyPDF2.PdfReader(pdf_file)
+    with open(os.path.join(folder, pdf_file_name), 'rb') as pdf_file:
+        pdf_reader = PyPDF2.PdfReader(pdf_file)
 
-    # Seiten zufällig mischen
-    num_pages = len(pdf_reader.pages)
-    pages = list(range(num_pages))
-    random.shuffle(pages)
+        # Seiten zufällig mischen
+        num_pages = len(pdf_reader.pages)
+        pages = list(range(num_pages))
+        random.shuffle(pages)
 
-    # Neue PDF-Datei erstellen
-    pdf_writer = PyPDF2.PdfWriter()
-    for page_num in pages:
-        page = pdf_reader.pages[page_num]
-        pdf_writer.add_page(page)
+        for page_num in pages:
+            page = pdf_reader.pages[page_num]
+            pdf_writer.add_page(page)
 
-    # Zufällig gemischte PDF-Datei speichern
-    shuffled_pdf_file_name = pdf_file_name.replace('.pdf', '') + '_Gemischt.pdf'
-    shuffled_pdf = open(os.path.join(folder, shuffled_pdf_file_name), 'wb')
+# Zufällig gemischte PDF-Datei speichern
+base_name, _ = os.path.splitext(pdf_file_name)
+shuffled_pdf_file_name = base_name + '_Gemischt.pdf'
+with open(os.path.join(folder, shuffled_pdf_file_name), 'wb') as shuffled_pdf:
     pdf_writer.write(shuffled_pdf)
-
-    # Ursprüngliche PDF-Datei schließen
-    pdf_file.close()
-    shuffled_pdf.close()
